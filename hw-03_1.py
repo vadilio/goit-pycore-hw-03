f1515bfe6174e3530ff8ea9fffc7ba748871b74f
vadilio <vlelin@gmail.com>
2025-03-26 00:57:09 +0100

    ready to check

--- hw-03_1.py
+++ hw-03_1.py
@@ -1,17 +1,30 @@
-print('HW3_1')
-from datetime import datetime
-# import datetime
+from datetime import datetime, timedelta
+import re
+
+# print('HW3_1')
+
+# Функція, яка розраховує кількість днів між заданою датою {Date} і поточною датою.
+
 
 def get_days_from_today(date: str) -> int:
-    pass
-    data_today = datetime.datetime.now().date()
+    data_today = datetime.now().date()
     try:
-        pass
-        target_date=datetime.datetime.strptime()
+        target_date = datetime.strptime(date, '%Y-%m-%d').date()
+        return (data_today - target_date).days
     except ValueError:
-        pass
-    
-data_today = datetime.datetime.now().date()
-#print(dir(datetime.datetime.strptime()))
-#print(data_today)
+        raise ValueError(
+            "Error: Incorrect Date Format. Must be like 'YYYY-MM-DD'")
 
+
+pattern = r"^\d{4}-\d{2}-\d{2}$"  # Задаємо патерн для перевірки формату
+flag_to_stop = True
+while flag_to_stop:
+    date = input(
+        "Enter target data in YYYY-MM-DD format (or exit - to cancel): ")
+    if re.match(pattern, date):
+        print(
+            f"The distance to target Date is: {get_days_from_today(date)} days.")
+    elif date == 'exit':
+        flag_to_stop = False
+    else:
+        print("Error: Incorrect Date Format. Try again..")
