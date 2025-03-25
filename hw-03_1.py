1a367a387f64cba8ca99d91f361bfba277acd0c2
vadilio <vlelin@gmail.com>
2025-03-25 23:34:46 +0100

    second commit

---
+++ hw-03_1.py
@@ -0,0 +1,17 @@
+print('HW3_1')
+from datetime import datetime
+# import datetime
+
+def get_days_from_today(date: str) -> int:
+    pass
+    data_today = datetime.datetime.now().date()
+    try:
+        pass
+        target_date=datetime.datetime.strptime()
+    except ValueError:
+        pass
+    
+data_today = datetime.datetime.now().date()
+#print(dir(datetime.datetime.strptime()))
+#print(data_today)
+
